2de3870caadcafed775eb86314f5381e163a41c3
S0meGuy0ne <40676584+S0meGuy0ne@users.noreply.github.com>
2018-07-02 10:47:06 -0400

    Update secret.py

--- secret.py
+++ secret.py
@@ -3,7 +3,6 @@ import asyncio
 import requests
 import random
 import string
-import os 
 from discord.ext import commands
 from discord.ext.commands import Bot
 bot = commands.Bot(command_prefix = '+')
@@ -11,22 +10,30 @@ bot = commands.Bot(command_prefix = '+')
 bot.ver_Count = 1
 bot.code_count = 1
 bot.v_users = []
-
-TOKEN=os.environ['BOT_TOKEN']
-
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    await bot.change_presence(game=discord.Game(name='+helpverify'), status=discord.Status('online'),afk=False)
 
-@bot.event
-async def on_server_join(server):
-    await bot.create_role(server,name="Verified")
 def random_generator(size=8, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for x in range(size))
-############################Sends the user a random code####################################
+    class_code = '+Verify_Code'
+    return class_code + ''.join(random.choice(chars) for x in range(size))
+
+@bot.command(pass_context=True)
+async def helpverify(ctx):
+    embed = discord.Embed(title = "How to verify:", description='', color=0x00ff00 )
+    embed.add_field(name="Step1:", value='Ask the bot for a random code by using the command "+send_code", The bot will DM you the code'+
+        'The code is valid for any server you are in. ', inline=False)
+    embed.add_field(name="Step2:", value='Copy the code and go on your brickplanet account setting', inline=False)
+    embed.add_field(name="Step3:", value='Insert the code in your profile blurd ', inline=False)
+    embed.add_field(name="Step4:", value='Go on the server you want to verify your self and say +verify <UserName>', inline=False)
+    embed,set_footer(text='For any bugs problems blame GreekSymbol#4686, in other words DM GreekSymbol#4686 and report it')    
+    await bot.send_message(ctx.message.author, embed=embed)
+
+##############################
 @bot.command(pass_context=True)
 async def send_code(ctx):
     global code_sent
@@ -37,67 +44,26 @@ async def send_code(ctx):
     await bot.send_message(ctx.message.channel, '{0.author.mention}'.format(ctx.message) + " Sliding into  your DMs :thumbsup:")
     bot.code_count = bot.code_count + 1
 
+
+
+##################################
 @bot.command(pass_context=True)
-async def helpverify(ctx):
-    embed = discord.Embed(title = "How to verify:", description='', color=0x00ff00 )
-    embed.add_field(name="Step1:", value='Ask the bot for a random code by using the command "+send_code", The bot will DM you the code'+
-        'The code is valid for any server you are in. ', inline=False)
-    embed.add_field(name='Step2', value='Go on brickplanet.com and login(*if you dont have an account sign up*)', inline=False)
-    embed.add_field(name='Step3', value='Instert the code that bot gave you in your Status', inline=False)
-    embed.add_field(name='Step4', value='Go on the server where you want to verify your self, and find the channel to verify your self',inline=False)
-    embed.add_field(name='Step5', value='In the channel/server say: +verify <username or userid> <code that was sent by the bot>')
-
-    await bot.send_message(ctx.message.author, embed=embed)
-
-
-
-@bot.command(pass_context=True)
-async def verify(ctx, user ,code):
+async def verify(ctx, user):
     try:
-        if ctx.message.author == code_for_user:
-            if code == code_sent:
-                r = requests.get("https://www.brickplanet.com/web-api/users/get-user/"+user)
-                data = r.json()
-                if data['Status'] == code:
-                    server = ctx.message.server
-                    role = discord.utils.get(server.roles, name="Verified")
-                    await bot.add_roles(ctx.message.author, role)
-                    await bot.change_nickname(ctx.message.author , nickname = data['Username'])
-                    await bot.delete_message(ctx.message)
-                    await bot.say('Verified!')
-                    bot.ver_Count = bot.ver_Count + 1
-                    user_fmt = ('**In server: **' +str(ctx.message.server)+ '**User: **' +str(ctx.message.author) + 'Verified account: ' +data['Username'])
-                    bot.v_users.append(user_fmt)
-                    print(data['Status'])
-                else:   
-                    await bot.say("Unable to verify user" +'{0.author.mention}'.format(ctx.message))
-            else:
-                await bot.say('Code is invalid'+'{0.author.mention}'.format(ctx.message))
-        else:
-            await bot.say('The code is not for you.'+'{0.author.mention}'.format(ctx.message))
+      r = requests.get("https://www.brickplanet.com/web-api/users/get-user/"+user)
+      data = r.json()
+      blurb = data['About']
+      if code_sent in blurb:
+          print("Code was found in the blurb!!")
+          server = ctx.message.server
+          role = discord.utils.get(server.roles, name="Verified")
+          await bot.add_roles(ctx.message.author, role)
+          await bot.change_nickname(ctx.message.author , nickname = data['Username'])
+          await bot.delete_message(ctx.message)
+          await bot.send_message(ctx.message.channel, '{0.author.mention}'.format(ctx.message) + "Verified!")
+      else:
+          await bot.delete_message(ctx.message)
+          await bot.send_message(ctx.message.channel, '{0.author.mention}'.format(ctx.message) + 'Code not found in blurb')
     except discord.Forbidden:
-        print("Error occured")
-        await bot.say("I do have privilege to edit that user's nickname")
-
-
-##################Logging Info##################
-
-@bot.command(pass_context=True)
-@commands.has_role("RANK")
-async def get_c_count(ctx):
-    await bot.say(bot.code_count)
-####################################
-@bot.command(pass_context=True)
-@commands.has_role("RANK")
-async def get_Ver_count(ctx):
-    await bot.say(bot.ver_Count)
-######################################
-@bot.command(pass_context=True)
-@commands.has_role("dev_Log_aCess_65")
-async def get_Ver_users(ctx):
-    user_Em = discord.Embed(title = 'Verified Users', description='list of verified users', color=0x00ff00)
-    for i in range(len(bot.v_users)):
-        user_Em.add_field(name='User {}'.format(i), value=bot.v_users[i])
-    await bot.say(embed=user_Em)
-####################################
-bot.run(TOKEN)
+        await bot.send_message(ctx.message.channel, '{0.author.mention}'.format(ctx.message) + 'Can not edit your information..You are too tall for me to reach')
+bot.run(TOEKN)
